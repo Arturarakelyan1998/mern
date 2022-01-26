73427c771c2f1dcbf2cbcd7378c72f4a4b616002
Arturarakelyan1998 <arturarakelyan089@gmail.com>
2022-01-26 22:44:05 +0400

    Creat user routes

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,35 +1,39 @@
-import React, {useState,useContext} from 'react'
-import {BrowserRouter,Link,Switch,Route } from 'react-router-dom'
+import React, {useState, useContext} from 'react'
+import {BrowserRouter, Link, Switch, Route} from 'react-router-dom'
 import axios from "axios";
 
-import { RiLockPasswordLine } from 'react-icons/ri';
-import { BiUser } from 'react-icons/bi';
+import {RiLockPasswordLine} from 'react-icons/ri';
+import {BiUser} from 'react-icons/bi';
 import './AuthPage.scss'
 import {AuthContext} from "../../context/AuthContext";
 
 const AuthPage = () => {
+    const [success, setSuccess] = useState('')
 
-    const [form,setForm]=useState({
-        email:'',
-        password:''
+
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
     })
 
-    const {login}=useContext(AuthContext)
-    const changeHandler =(event)=>{
-        setForm({...form,[event.target.name]:event.target.value})
+    const {login} = useContext(AuthContext)
+    const changeHandler = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
     }
-    const registerHandler = async()=>{
-        try{
-            await axios.post('/api/auth/registration',{...form},{
-                headers:{
-                    'Content-Type':'application/json'
+    const registerHandler = async () => {
+        try {
+            await axios.post('/api/auth/registration', {...form}, {
+                headers: {
+                    'Content-Type': 'application/json'
                 }
-            }).then(response=>console.log( response))
-        }catch(error){
-            console.log(error)
+            }).then(response =>
+                setSuccess(response.data.message)
+            )
+        } catch (error) {
+            console.log(error, "Error ")
         }
     }
-    const loginHandler = async ()=>{
+    const loginHandler = async () => {
         try {
 
             await axios.post('/api/auth/login', {...form}, {
@@ -46,65 +50,76 @@ const AuthPage = () => {
     }
 
 
-        return (
-            <BrowserRouter>
-                <Switch>
-            <>
-                <div className="container">
-                    <div className="auth-page">
+    return (
+        <BrowserRouter>
+            <Switch>
+                <>
+                    <div className="container">
+                        <div className="auth-page">
 
-                        <Route path="/login">
-                        <h3> Authentication</h3>
-                        <form className="form form-login" onSubmit={e=>e.preventDefault()}>
-                            <div className="row">
-                                <div className="input-filed col s12">
-                                    <BiUser />
-                                    <input type="email" name="email"  placeholder="Email" onChange={changeHandler}/>
-                                </div>
-                                <div className="input-filed col s12">
-                                    <RiLockPasswordLine />
-                                    <input type="password" name="password"  placeholder="Password" onChange={changeHandler}/>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <button className=" waves-effect waves-light btn btn blue" onClick={loginHandler}> Log In</button>
-                                <Link to="/registration" className="btn-outline btn-reg" >Haven't account ?</Link>
-                            </div>
-                        </form>
-                        </Route>
+                            <Route path="/login">
+                                <h3> Authentication</h3>
+                                <form className="form form-login" onSubmit={e => e.preventDefault()}>
+                                    <div className="row">
+                                        <div className="input-filed col s12">
+                                            <BiUser/>
+                                            <input type="email" name="email" placeholder="Email"
+                                                   onChange={changeHandler}/>
+                                        </div>
+                                        <div className="input-filed col s12">
+                                            <RiLockPasswordLine/>
+                                            <input type="password" name="password" placeholder="Password"
+                                                   onChange={changeHandler}/>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <button className=" waves-effect waves-light btn btn blue"
+                                                onClick={loginHandler}> Log In
+                                        </button>
+                                        <Link to="/registration" className="btn-outline btn-reg">Haven't account
+                                            ?</Link>
+                                    </div>
+                                </form>
+                            </Route>
 
-                        <Route path="/registration">
-                        <h3> Registration</h3>
-                        <form className="form form-login" onSubmit={e => e.preventDefault()}>
+                            <Route path="/registration">
+                                <h3> Registration</h3>
+                                <form className="form form-login" onSubmit={e => e.preventDefault()}>
 
 
-                            <div className="row">
-                                <div className="input-filed col s12">
-                                    <input type="text" name="firstname"  placeholder="Firstname" onChange={changeHandler}/>
-                                </div>
-                                <div className="input-filed col s12">
-                                    <input type="text" name="lastname"  placeholder="Lastname" onChange={changeHandler}/>
-                                </div>
-                                <div className="input-filed col s12">
-                                    <input type="email" name="email"  placeholder="Email" onChange={changeHandler}/>
-                                </div>
-                                <div className="input-filed col s12">
-                                    <input type="password" name="password"  placeholder="Password" onChange={changeHandler}/>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <button className=" waves-effect waves-light btn btn blue "  onClick={registerHandler} > Registration</button>
-                                <Link to="/login" className="btn-outline btn-reg">already have account ?</Link>
-                            </div>
-                        </form>
-                    </Route>
+                                    <div className="row">
+                                        <div className="input-filed col s12">
+                                            <input type="text" name="firstname" placeholder="Firstname"
+                                                   onChange={changeHandler}/>
+                                        </div>
+                                        <div className="input-filed col s12">
+                                            <input type="text" name="lastname" placeholder="Lastname"
+                                                   onChange={changeHandler}/>
+                                        </div>
+                                        <div className="input-filed col s12">
+                                            <input type="email" name="email" placeholder="Email"
+                                                   onChange={changeHandler}/>
+                                        </div>
+                                        <div className="input-filed col s12">
+                                            <input type="password" name="password" placeholder="Password"
+                                                   onChange={changeHandler}/>
+                                        </div>
+                                    </div>
+                                    {success ?  <h1 style={{color:"green",fontSize:24}}>{success}</h1>:(<div className="row">
+                                        <button className=" waves-effect waves-light btn btn blue "
+                                                onClick={registerHandler}> Registration
+                                        </button>
+                                        <Link to="/login" className="btn-outline btn-reg">already have account ?</Link>
+                                    </div>)}
+                                </form>
+                            </Route>
+                        </div>
                     </div>
-                </div>
-            </>
-                </Switch>
-                </BrowserRouter>
+                </>
+            </Switch>
+        </BrowserRouter>
 
-                )
+    )
 }
 
 export default AuthPage;
--- server/routes/auth.routs.js
+++ server/routes/auth.routs.js
@@ -1,88 +1,94 @@
-const {Router}=require('express')
+const {Router} = require('express')
 const router = Router()
 const User = require('../models/User')
-const{check, validationResult}= require('express-validator')
+const {check, validationResult} = require('express-validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
 
 router.post('/registration',
     [
-        check('firstname','Firstname can not be empty').notEmpty(),
-        check('lastname','Lastname can not be empty').notEmpty(),
-        check('email','Not correct email').isEmail(),
-        check('password','Not correct password').isLength({min:6}),
+        check('firstname', 'Firstname can not be empty').notEmpty(),
+        check('lastname', 'Lastname can not be empty').notEmpty(),
+        check('email', 'Not correct email').isEmail(),
+        check('password', 'Not correct password').isLength({min: 6}),
     ],
-    async (req,res)=>{
-    try {
-        const errors= validationResult(req)
-        if(!errors.isEmpty()){
-            return res.status(400).json({
-                errors:errors.array(),
-                message:'not correct data upon registration'
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'not correct data upon registration'
+                })
+            }
+            const {firstname, lastname, email, password} = req.body
+            const isUsed = await User.findOne({email})
+            if (isUsed) {
+                return res.status(300).json({message: 'This email already registered'})
+            }
+            const hashedPassword =await bcrypt.hash(password,12)
+
+            const user = new User({
+                firstname,
+                lastname,
+                email,
+                password:hashedPassword
             })
-        }
-        const {firstname,lastname,email, password}= req.body
-        const  isUsed=await User.findOne({email})
-        if(isUsed){
-           return  res.status(300).json({message:'This email already registered'})
-        }
-        // const hashedPassword =await bcrypt.hash(password,12)
+            await user.save()
 
-        const user = new User({
-            firstname,
-            lastname,
-            email,
-            password
-        })
-        await user.save()
-
-        res.status(201).json({message:'user created successfully'})
-    }
-    catch(error)  {
-        console.log(error);
-    }
-    })
+            res.status(201).json({message: 'user created successfully'})
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
 router.post('/login',
     [
-        check('email','Not correct email').isEmail(),
-        check('password','Not correct password').exists(),
+        check('email', 'Not correct email').isEmail(),
+        check('password', 'Not correct password').exists(),
     ],
-    async (req,res)=>{
-    try {
-        const errors= validationResult(req)
-        if(!errors.isEmpty()){
-            return res.status(400).json({
-                errors:errors.array(),
-                message:'not correct data upon login'
-            })
-        }
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'not correct data upon login'
+                })
+            }
 
-        const {email,password }= req.body
+            const {email, password} = req.body
 
-        const user = await User.findOne({email})
-         if(! user )
-         {
-             return res.status(400).json({message:'email is false '})
-         }
-         // const isMatch=bcrypt.compare(password,user.password)
-        if(password!=user.password){
-            return res.status(400).json({message:'password is false '})
-        }
+            const user = await User.findOne({email})
+            if (!user) {
+                return res.status(400).json({message: 'email is false '})
+            }
+            const isMatch=await bcrypt.compare(password,user.password)
+            if (!isMatch) {
+                return res.status(400).json({message: 'password is false '})
+            }
 
-        const jwtSecret="a1b2c3d4e5f6g7"
+            const jwtSecret = "a1b2c3d4e5f6g7"
 
-        const token = jwt.sign(
-            {userId:user.id},
-            jwtSecret,
-            {expiresIn: '1h'}
+            const token = jwt.sign(
+                {userId: user.id},
+                jwtSecret,
+                {expiresIn: '1h'}
             )
 
-        res.json({token,userId:user.id})
+            res.json({token, userId: user.id})
 
-    }catch(error){
-        console.log(error);
-    }
-    })
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+
+
+
+router.get('/date', function (req, res) {
+    let x = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+    res.send(x)
+})
 
 module.exports = router
